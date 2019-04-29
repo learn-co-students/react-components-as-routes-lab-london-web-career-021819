60bb26287a9e2519409fa40444b12d3a5e5d3379
Alex Bujenita <dekc89@gmail.com>
2019-04-29 12:59:55 +0100

    done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        { movies.map(movie => {
+          return (
+            <div>
+              Title: {movie.title}, Running time: {movie.time} minutes.
+              <ul>
+                Genres:
+                {movie.genres.map(genre => <li>{genre}</li>)}
+              </ul>
+            </div>
+          )
+        }) }
     </div>
   );
 };
