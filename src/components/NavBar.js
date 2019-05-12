3323fe72b8e20a3de1915ead6803df69379c3388
louieogrady <l.ogrady@gmail.com>
2019-05-12 19:01:08 +0100

    fin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
+
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink to="/actors">Actors</NavLink>
+      <br />
+      <NavLink to="/directors">Directors</NavLink>
+      <br />
+      <NavLink to="/movies">Movies</NavLink>
+      <br />
+      <NavLink to="/">Home</NavLink>
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import Actors from '../components/Actors';
-import Directors from '../components/Directors';
-import Movies from '../components/Movies';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import Actors from "../components/Actors";
+import Directors from "../components/Directors";
+import Movies from "../components/Movies";
 
-
-const App = (props) => {
+const App = props => {
   return (
-    <Router>
-      {/*{code here}*/}
-    </Router>
+    <div className="App">
+      <div>
+        <NavBar />
+      </div>
+
+      <div>
+        <Switch>
+          <Route path="/actors" render={() => <Actors />} />
+          <Route path="/directors" render={() => <Directors />} />
+          <Route path="/movies" render={() => <Movies />} />
+          <Route path="/" component={() => <Home />} />
+        </Switch>
+      </div>
+    </div>
   );
 };
 
-export default App
+export default App;
